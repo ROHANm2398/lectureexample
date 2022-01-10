0bf0b73b6a739fdc908cd9b34165fea6e986cbe7
Rohan <rmandavkar98@gmail.com>
2022-01-10 17:54:11 +0530

    final version

---
+++ src/main/java/org/examplee/java_jenkins_in_dockerr/main.java
@@ -0,0 +1,16 @@
+package org.examplee.java_jenkins_in_dockerr;
+
+public class main {
+	public static boolean checkIfInputIsAnEvenNumber(int
+			number){
+			return number % 2 == 0;
+			}
+			
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
